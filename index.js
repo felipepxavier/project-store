b0d6523a73a7fc48d0f0b0e77d51088fd14753ee
TANRAC SALDANHA <tanrac.saldanha@gravatai.local>
2019-10-15 15:06:00 -0300

    finalizado

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const server = express();
 
 server.use(express.json());
 
-const projects = [{ id: "1", title: 'Novo projeto', tasks: [] }];
-
+//{ id: "1", title: 'Novo projeto', tasks: [] }
+const projects = [];
 
 server.post('/projects', (req, res) => {
-    const { id, title, tasks } = req.body;
-    projects.push( {id, title, tasks} );
+    const { id, title } = req.body;
+
+    const project = {
+            id,
+            title,
+            tasks: [],
+        }
     
-    return res.json(projects);
-})
+    projects.push(project);
+    
+    return res.json(project);
+});
 
 server.get('/projects', (req, res) =>{
     return res.json(projects)
@@ -24,14 +31,27 @@ server.put('/projects/:id', (req, res) => {
     const project = projects.find(p => p.id == id);
 
     project.title = title;
-    return res.json(projects);
+    return res.json(project);
 })
 
+server.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const project = projects.find(p => p.id == id);
 
+    projects.splice( project, 1);
 
+    return res.send();
+})
 
+server.post('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    const project = projects.find(p => p.id == id);
 
+    project.tasks.push(title);
 
+    return res.json(project);
+})
 
 
 server.listen(3000);
